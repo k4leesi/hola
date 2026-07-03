4ac66ee94c1041e96a9bf0c43e71bc6b2c76d061
k4leesi <298223328+k4leesi@users.noreply.github.com>
2026-07-03 07:10:11 -0500

    stepGithub

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,9 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
      
-
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Hello world..!  I´m Kali
           </h1>
-
-
 
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -42,7 +39,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/k4leesi"
             target="_blank"
             rel="noopener noreferrer"
           >
